a2463c522e18da53a9a3c6c38191dcc29030c168
Ngo Ngoc Vu <ngongocvu668@gmail.com>
2023-11-04 10:11:31 +0700

    first commit

---
+++ src/main/java/vn/edu/iuh/fit/www_lab_07/WwwLab07Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_lab_07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwwLab07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwwLab07Application.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/iuh/fit/www_lab_07/WwwLab07ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.www_lab_07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WwwLab07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
